59db88477fcc6b093fe0762086be61349bfd87eb
alidezmo-pro <ali.dezmo@gmail.com>
2026-07-30 16:13:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// 1. تحديد أرقام الحجر
-let topNumber = 6;
-let bottomNumber = 6; 
+// 1. إنشاء مصفوفة (Array) فارغة لتخزين الـ 28 حجراً
+let dominoDeck = [];
 
-// 2. الوصول إلى الطاولة (الصندوق الفارغ في HTML)
+// 2. استخدام حلقة تكرار مضاعفة (Nested Loops) لإنشاء الأحجار بدون تكرار
+// الرقم العلوي يبدأ من 0 إلى 6
+for (let top = 0; top <= 6; top++) {
+    // الرقم السفلي يبدأ من قيمة الرقم العلوي لضمان عدم تكرار الأحجار (مثل 0-1 و 1-0)
+    for (let bottom = top; bottom <= 6; bottom++) {
+        // إضافة الحجر إلى القائمة ككائن (Object) يحتوي على الرقمين
+        dominoDeck.push({ top: top, bottom: bottom });
+    }
+}
+
+// 3. تحديد عنصر الطاولة من صفحة HTML
 let board = document.getElementById("game-board");
 
-// 3. رسم الحجر بداخل الطاولة
-board.innerHTML = `
-    <div class="domino-piece">
-        <div>${topNumber}</div>
-        <div class="divider"></div>
-        <div>${bottomNumber}</div>
-    </div>
-`;
+// 4. تفريغ الطاولة من أي محتوى سابق
+board.innerHTML = "";
+
+// 5. تكرار عرض كل حجر داخل المصفوفة على شاشة الطاولة
+dominoDeck.forEach(piece => {
+    board.innerHTML += `
+        <div class="domino-piece">
+            <div>${piece.top}</div>
+            <div class="divider"></div>
+            <div>${piece.bottom}</div>
+        </div>
+    `;
+});
