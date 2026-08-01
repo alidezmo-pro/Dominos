62b9af4c9c073e17e3fa032899492f31027ade1e
alidezmo-pro <ali.dezmo@gmail.com>
2026-08-01 17:43:51 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// main.js
+import { setSelectPlayers, setSelectScore, showStartModal } from './ui.js';
+import { createRoom, joinRoom } from './firebase.js';
+import { selectGameMode, drawFromBoneyard, selectBoardEnd } from './logic.js';
+
+// ربط الوظائف للـ HTML Buttons
+window.setSelectPlayers = setSelectPlayers;
+window.setSelectScore = setSelectScore;
+window.createRoom = createRoom;
+window.joinRoom = joinRoom;
+window.selectGameMode = selectGameMode;
+window.drawFromBoneyard = drawFromBoneyard;
+window.selectBoardEnd = selectBoardEnd;
+window.showStartModal = showStartModal;
+
+window.onload = function() { 
+    showStartModal(); 
+};
