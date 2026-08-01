c7ec069e536170053fc51de91834e24caa51fad8
alidezmo-pro <ali.dezmo@gmail.com>
2026-08-01 17:48:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 import { setSelectPlayers, setSelectScore, showStartModal } from './ui.js';
 import { createRoom, joinRoom } from './firebase.js';
 import { selectGameMode, drawFromBoneyard, selectBoardEnd } from './logic.js';
+// في أعلى ملف main.js
+import { state } from './state.js';
+
+// مع بقية المتغيرات التي يتم ربطها بـ window
+window.state = state;
 
 // ربط الوظائف للـ HTML Buttons
 window.setSelectPlayers = setSelectPlayers;
