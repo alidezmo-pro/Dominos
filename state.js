64e4bf675e99d709034830a73de924b77066eff1
alidezmo-pro <ali.dezmo@gmail.com>
2026-08-01 17:39:58 +0300

    Create state.js

---
+++ state.js
@@ -0,0 +1,34 @@
+// state.js
+export const state = {
+    // Online Configuration
+    roomId: null,
+    playerRole: null, 
+    isOnline: false, 
+    roomMaxPlayers: 2,
+    targetScore: 100,
+    roomScores: { host: 0, guest1: 0, guest2: 0 },
+    lastAlert: null,
+
+    // Lobby Selections
+    selectedPlayersCount: 2,
+    selectedTargetScore: 100,
+
+    // Local / Game Board State
+    fullSet: [], 
+    boneyard: [], 
+    playerHand: [], 
+    comp1Hand: [], 
+    comp2Hand: [], 
+    boardChain: [],
+    gameMode: 2, 
+    currentTurn: 'player', 
+    selectedTileIndex: null,
+    leftEndValue: null, 
+    rightEndValue: null, 
+    isGameOver: false, 
+    centerTileIndex: 0,
+    
+    // Timer
+    turnTimerInterval: null, 
+    timeLeft: 20
+};
